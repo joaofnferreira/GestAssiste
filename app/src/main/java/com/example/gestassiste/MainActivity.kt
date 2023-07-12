6cd9e36a727b968bfbedcf7f619964b83a362670
aluno20230 <aluno20230@ipt.pt>
2023-07-12 21:59:20 +0100

    Ligação ao firebase
    Ligação ao firebase

--- app/src/main/java/com/example/gestassiste/MainActivity.kt
+++ app/src/main/java/com/example/gestassiste/MainActivity.kt
@@ -4,15 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class MainActivity : AppCompatActivity() {
-    //// camera
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
 
         val buttonClick = findViewById<Button>(R.id.criarassistencia)
 
