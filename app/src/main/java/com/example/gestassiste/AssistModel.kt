b770d6598155a4da7b5f083f9434dcc329f0483d
aluno20230 <aluno20230@ipt.pt>
2023-08-12 16:15:32 +0100

    Inserção com sucesso da secção equipamento
    Inserção com sucesso da secção equipamento

--- app/src/main/java/com/example/gestassiste/AssistModel.kt
+++ app/src/main/java/com/example/gestassiste/AssistModel.kt
@@ -12,6 +12,8 @@ data class AssistModel (
         val telemovel: String? = null,
         val email: String? = null,
         //equipamento
+        val equipamento: String? = null,
+        val modelo: String? = null,
         val serial: String? = null
 )
 
--- app/src/main/java/com/example/gestassiste/Assistencia.kt
+++ app/src/main/java/com/example/gestassiste/Assistencia.kt
@@ -1,9 +1,18 @@
 package com.example.gestassiste
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -21,8 +30,12 @@ class Assistencia : AppCompatActivity() {
     private lateinit var ctelemovel: EditText
     private lateinit var cnome: EditText
 
+    lateinit var photo_button: Button
+    lateinit var imageView: ImageView
+
+    private lateinit var cequipamento: EditText
+    private lateinit var cmodelo: EditText
     private lateinit var cserial: EditText
-    private lateinit var cserialteste: TextView
 
 
     private lateinit var dbRef: DatabaseReference
@@ -42,12 +55,22 @@ class Assistencia : AppCompatActivity() {
         ctelemovel = findViewById(R.id.ctelemovel)
         cemail = findViewById(R.id.cemail)
 
-        //cserialteste = view.findViewById(R.id.cserialteste)
+        cequipamento = findViewById(R.id.cequipamento)
+        cmodelo = findViewById(R.id.cmodelo)
+        cserial = findViewById(R.id.cserial)
+
+
+        photo_button = findViewById(R.id.photo_button)
+        imageView = findViewById(R.id.imageView)
 
         dbRef = FirebaseDatabase.getInstance().getReference("Assist")
 
         button_save.setOnClickListener{
             saveEquipamento()
+        }
+
+        photo_button.setOnClickListener {
+            capturePhoto()
         }
 
 
@@ -71,8 +94,9 @@ class Assistencia : AppCompatActivity() {
         val cemail1 = cemail.text.toString()
 
         //fragmento equipamento
-
-        //val cserial1 = cserialteste.text.toString()
+        val cequipamento1 = cequipamento.text.toString()
+        val cmodelo1 = cmodelo.text.toString()
+        val cserial1 = cserial.text.toString()
 
         if (editTextDate1.isEmpty()){
             editTextDate.error = "Insira o valor"
@@ -110,10 +134,26 @@ class Assistencia : AppCompatActivity() {
             return
         }
 
+        //fragmento equipamento
+        if (cequipamento1.isEmpty()){
+            cequipamento.error = "Insira o valor"
+            return
+        }
+
+        if (cmodelo1.isEmpty()){
+            cmodelo.error = "Insira o valor"
+            return
+        }
+
+        if (cserial1.isEmpty()){
+            cserial.error = "Insira o valor"
+            return
+        }
+
         val assistID = dbRef.push().key!!
 
-        val assist = AssistModel(assistID, editTextDate1, cproblema1, cproblema21, corcamento1, cnome1, ctelemovel1, cemail1)
-        //,cserial1
+        val assist = AssistModel(assistID, editTextDate1, cproblema1, cproblema21, corcamento1, cnome1, ctelemovel1, cemail1, cequipamento1, cmodelo1, cserial1)
+
 
         dbRef.child(assistID).setValue(assist)
             .addOnCompleteListener{
@@ -131,7 +171,9 @@ class Assistencia : AppCompatActivity() {
                 cemail.text.clear()
 
                 //fragmento equipamento
-                //cserial.text.clear()
+                cequipamento.text.clear()
+                cmodelo.text.clear()
+                cserial.text.clear()
 
 
             }.addOnFailureListener {
@@ -139,4 +181,38 @@ class Assistencia : AppCompatActivity() {
             }
 
     }
+
+    ///////////////////////////////////////////////////////
+    fun capturePhoto() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        resultLauncher.launch(cameraIntent)
+    }
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            imageView.setImageBitmap(data?.extras?.get("data") as Bitmap)
+        }
+    }
+
+    val contentValues = ContentValues().apply {
+        //put(MediaStore.MediaColumns.DISPLAY_NAME)
+        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+        }
+    }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+
 }
